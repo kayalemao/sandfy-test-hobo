3bbaa3bc0b072da1db28ff0e95c9d2afa92f0fa5
kayalemao <kayalemao@online.de>
2010-11-20 03:54:02 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandfyTestHobo::Application.config.secret_token = '69b526f11ebe2ad0acea72a5dd2cd2f655d2e6dcf5dc4ed22a45041a03db8fb6c6fe4614d1ca78a2eb4cf3c1f99d9d3b0a1f00df76669ed1628d712e2745b7e1'
