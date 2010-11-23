be28d860127568bef97b1455093482376f9b9396
kayalemao <kayalemao@online.de>
2010-11-23 23:41:47 +0100

    layout changes being done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include Hobo::Controller::AuthenticationSupport
+  before_filter :except => :login do
+     login_required unless User.count == 0
+  end
 end
